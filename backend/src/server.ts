751db07fbc22510d9fb49f8f6b291d45e94f836b
Md Sahi Alam Khan <sahilkhan433180@gmail.com>
2024-06-10 21:35:03 +0530

    changes

--- backend/src/server.ts
+++
@@ -1,8 +0,0 @@
-import { app } from "./app.js";
-import { connectDB } from "./data/database.js";
-
-connectDB();
-
-app.listen(process.env.PORT, () => {
-  console.log(`Running on port ${process.env.PORT}...`);
-});
